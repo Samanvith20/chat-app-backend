0fd6e7e62d67bc98b3f77082126ff8b389b2af45
Samanvith <Samanvith2005@gmail.com>
2025-09-17 18:50:01 +0530

    established the connection and connect to mongodb

---
+++ routes/msgs.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import getMsgsForConversation from "../controllers/msgs.contoller.js"
+
+const router = express.Router();
+
+router.get('/', getMsgsForConversation);
+
+export default router;
